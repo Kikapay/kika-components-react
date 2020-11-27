f886b7f5c6fd8b81704681c4ed9c210f4fc451dc
leo-kika <leo.thesen@kikapay.com>
2020-11-27 15:44:38 +0200

    [ADDED] default react app and example from create-react-library

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from '@kikapay/kika-components-react'
+import '@kikapay/kika-components-react/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App

dbe23a843fba26935b868d4cf16dc6426ace51cc
leo-kika <leo.thesen@kikapay.com>
2020-11-27 15:56:05 +0200

    [REMOVED] unrequired example and imported the kika-component-react library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { ExampleComponent } from '@kikapay/kika-components-react'
+import '@kikapay/kika-components-react/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Kika component library" />
 }
 
-export default App;
+export default App

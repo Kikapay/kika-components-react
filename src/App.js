95a87c5f6933d7ef7a12f0fab78f46dd50119dfa
leothesen <leo@mohara.co>
2020-12-07 14:16:16 +0200

    updated components
    Removed the example component and  added InfoBox along with mock data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react'
 
-import { ExampleComponent, Checkbox } from '@kikapay/kika-components-react'
+import { Checkbox, InfoBox } from '@kikapay/kika-components-react'
 import '@kikapay/kika-components-react/dist/index.css'
+
+// Mock data
+import { infoBoxData } from './mockData/infoBox'
 
 const App = () => {
   return <div> 
+    <h1>KikaComponents</h1>
+
+    <h2>Checkbox</h2>
     <Checkbox />
-    <ExampleComponent text="Create React Library Example 😄" />
+
+    <h2>Info Box</h2>
+    <InfoBox 
+      infoBoxDetails={infoBoxData}
+      merchantColor="#123321"
+    />
   </div>
 }
 
